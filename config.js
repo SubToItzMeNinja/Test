6797aae0b014367516177feb1f509e91ffbc9fee
SubToItzMeNinja <164725630+SubToItzMeNinja@users.noreply.github.com>
2024-04-24 00:02:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    'token': '', // Your bot token, activate all bot intents in discord developer portal.
-    'clientid': '', // Your discord bot id.
-    'guildid': '', // Id of a private server to load the bot's slash commands.
-    'ownerid': [''], // Add discord ids to the array to access the commands.
+    'token': 'MTE4OTg5Mzc5NzYyMzExMTc2NA.GsDEhP.G92k0OJBa_TowgnSRxABh8rUYehA99pUsEHaVM', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '1189893797623111764', // Your discord bot id.
+    'guildid': '973247822390505542', // Id of a private server to load the bot's slash commands.
+    'ownerid': ['782484813324091393'], // Add discord ids to the array to access the commands.
     'webhook': '' // Your discord webhook url.
 }
